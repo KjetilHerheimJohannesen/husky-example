1b7d0e81bf6156afa2329500de23c7643d70dc61
Kjetil Johannesen <123094872+KjetilHerheimJohannesen@users.noreply.github.com>
2025-07-03 10:17:19 +0200

    Test commit

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  {
+    ignores: ['node_modules'],
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...prettier.rules,
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      'no-unused-vars': 'warn',
+      eqeqeq: 'warn',
+    },
+  },
+];
---
+++ src/index.js
@@ -0,0 +1,8 @@
+let i = 0;
+let j = 2;
+
+console.log(j);
+
+if (i == j) {
+  console.log('Test');
+}
